6e6165595af93978bfd4e90c5fa87ef3357033c2
LucDub-code <lucdubcom@gmail.com>
2025-11-13 16:54:01 +0100

    design system creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Poppins } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: "PixlMe",
   description: "",
+  icons: {
+    icon: "/icons/pixlme-logo.png",
+  },
 };
+
+const poppins = Poppins({ subsets: ['latin'], weight: '400' })
 
 export default function RootLayout({
   children,
@@ -13,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+// import { Jersey_10 } from 'next/font/google'
 
+// const jersey = Jersey_10({ subsets: ['latin'], weight: '400' })
 
 export default function Home() {
   return (
-    <div>
+    <div className='min-h-screen grid grid-cols-5'>
+
+      <div className='col-span-3 flex items-center justify-center bg-linear-to-t
+       from-mauve from-0%
+       via-pink via-20%
+       to-yellow to-70%
+       '>
+
+      </div>
+
+      <div className='col-span-2 flex items-center justify-center'>
+        <p>FORM</p>
+      </div>
+
     </div>
   );
 }
